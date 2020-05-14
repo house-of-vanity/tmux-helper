1030e69932ab2d0884f9bd5bc955b4e86c6ca2b9
AB <ultradesu@hexor.ru>
2020-05-14 21:53:41 +0300

    Hotfix

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,8 @@ fn main() {
         config::Action::Cpu => utils::cpu_load_bar(15, &conf),
         config::Action::Mem => utils::mem_load_bar(15, &conf),
         config::Action::Mpris => utils::mpris(&conf),
-        config::Action::Utctime => utils::get_time(true, ""),
-        config::Action::Localtime => utils::get_time(false, ""),
+        config::Action::Utctime => utils::get_time(true, conf.ut_format),
+        config::Action::Localtime => utils::get_time(false, conf.lt_format),
         config::Action::Mpd => utils::mpd(&conf),
     }
 }
